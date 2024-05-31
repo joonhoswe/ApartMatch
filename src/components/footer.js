6559456ab3bc02ae964f68f6f6570ca013cb3811
joonhoswe <joonhoswe@gmail.com>
2024-05-31 10:54:41 +0900

    mobile resizing improvements, still has small bugs

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,31 +4,31 @@ import Logo from '../assets/logo.png';
 
 export default function Footer() {
     return (
-        <div className="bg-red-500 text-white flex flex-col md:flex-row md:space-x-16 lg:space-x-32 max-w-full px-8 md:px-16 py-8 space-y-8 md:space-y-0">
+        <div className="bg-red-500 text-white flex flex-col md:flex-row md:space-x-16 lg:space-x-32 min-h-[100px]lg:h-56 w-full px-8 md:px-16 py-8 space-y-8">
             <div className="flex justify-center md:justify-start">
-                <div className="relative w-64 h-24">
-                    <Image src={Logo} alt="TutorHive Logo" layout="fill" objectFit="contain" />
+                <div className="relative w-48 h-16 md:w-64 md:h-24">
+                    <Image src={Logo} alt="ApartMatch Logo" layout="fill" objectFit="contain" />
                 </div>
             </div>
             
             {/* contacts */}
-            <div className="flex flex-col space-y-2 pt-3 items-center md:items-start">
-                <h1 className="text-2xl"> CONTACT </h1>
-                <p> apartmatchco@gmail.com </p>
+            <div className="flex flex-col space-y-2 items-center md:items-start">
+                <h1 className="text-base md:text-2xl"> CONTACT </h1>
+                <p className="text-sm md:text-base"> apartmatchco@gmail.com </p>
             </div>
 
             {/* about */}
-            <div className="flex flex-col space-y-2 pt-3 items-center md:items-start">
-                <h1 className="text-2xl"> ABOUT </h1>
-                <p> Privacy Policy </p>
-                <p> Terms of Service </p>
+            <div className="flex flex-col space-y-2 items-center md:items-start">
+                <h1 className="text-base md:text-2xl"> ABOUT </h1>
+                <p className="text-sm md:text-base"> Privacy Policy </p>
+                <p className="text-sm md:text-base"> Terms of Service </p>
             </div>
 
             {/* socials */}
-            <div className="flex flex-col space-y-2 pt-3 items-center md:items-start">
-                <h1 className="text-2xl"> SOCIALS </h1>
-                <p> Instagram: </p>
-                <p> TikTok </p>
+            <div className="flex flex-col space-y-2 items-center md:items-start">
+                <h1 className="text-base md:text-2xl"> SOCIALS </h1>
+                <p className="text-sm md:text-base"> Instagram: </p>
+                <p className="text-sm md:text-base"> TikTok </p>
             </div>
         </div>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -7,18 +7,17 @@ export default function Home() {
   const infoCards = [
     { 
         heading: '❌ Facebook housing groups are inefficient', 
-        description: 'Say goodbye to sifting through outdated and irrelevant Facebook posts. Our platform offers real-time listings, ensuring you always see the most current and available housing options.' 
+        description: 'Say goodbye to sifting through outdated and irrelevant Facebook posts. ApartMatch offers real-time listings, ensuring you always see the most current and available housing options.' 
     },
     { 
       heading: '✅ See openings for apartments in real time', 
       description: 'Stay up-to-date with real-time availability. Instantly view and join partially filled apartments, making it easier and faster to save money while finding your ideal living situation.' 
     },
     { 
-        heading: '🔒 Only students with valid .edu emails are allowed', 
+        heading: '🔒 Only students with .edu emails are allowed', 
         description: 'Safety and trust are our priorities. Only users with valid .edu email addresses can join, guaranteeing that you are matched with verified college students from your own institution.' 
     },
 ];
-
 
   return (
     <div className="flex flex-col items-center w-full h-screen text-white font-bold">
@@ -30,28 +29,27 @@ export default function Home() {
           backgroundPosition: 'center'
         }}
       >
-        <div className='text-4xl flex flex-col space-y-6 items-center'>
+        <div className='text-md sm:text-3xl md:text-4xl flex flex-col space-y-3 md:space-y-6 items-center'>
           <h1> No more Facebook threads. </h1>
           <h1> Instantly <span className='text-red-500 underline'> match</span> into apartments. </h1>
         </div>
 
-        <div className='flex justify-end bg-white h-12 w-1/2 rounded-2xl'>
+        <div className='flex justify-end bg-white h-10 md:h-12 w-3/4 md:w-1/2 rounded-lg md:rounded-2xl'>
           <input 
-            className={`h-full w-5/6 rounded-l-2xl px-4 text-black outline-none ${school === '' ? 'text-gray-400' : ''}`} 
+            className={`h-full w-5/6 rounded-l-lg md:rounded-l-2xl px-4 text-black outline-none text-xs sm:text-sm lg:text-base ${school === '' ? 'text-gray-400' : ''}`} 
             placeholder="Ex: Carnegie Mellon University"
           />
-          <button onClick={(e) => setSchool(e.target.value)} className='bg-red-500 h-full w-1/4 rounded-r-2xl hover:bg-red-600 transition ease-in-out duration-300'> Find Homes! </button>
+          <button onClick={(e) => setSchool(e.target.value)} className='bg-red-500 h-full w-1/4 rounded-r-lg md:rounded-r-2xl hover:bg-red-600 transition ease-in-out duration-300 text-xs sm:text-sm lg:text-base'> Find Homes! </button>
         </div>
       </div>
 
-      <div className='bg-gray-200 h-4/5 w-full flex flex-row space-x-12 items-center justify-center'>
-        {infoCards.map((card) => (
-          <div className='bg-white text-black h-2/3 w-1/4 rounded-2xl flex flex-col items-center space-y-4 p-4 shadow-2xl'>
-            <h1 className='text-2xl'> {card.heading} </h1>
-            <p> {card.description} </p>
+      <div className='bg-gray-200 h-4/5 w-full flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-12 items-center justify-center py-10'>
+        {infoCards.map((card, index) => (
+          <div key={index} className='bg-white text-black sm:h-2/3 md:h-4/5 w-3/4 md:w-1/4 rounded-2xl flex flex-col items-center space-y-4 p-3 md:p-6 shadow-2xl'>
+            <h1 className='text-sm md:text-xl lg:text-2xl'> {card.heading} </h1>
+            <p className='text-xs md:text-sm lg:text-base'> {card.description} </p>
           </div>
         ))}
-
       </div>
     </div>
   );
