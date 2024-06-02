65e1d10025c3e3683b781b6a56147a086a0c2cf7
joonhoswe <joonhoswe@gmail.com>
2024-06-02 23:53:36 +0900

    migrate to clerk auth

--- src/pages/postListing.js
+++ src/pages/postListing.js
@@ -1,4 +1,6 @@
 import { React, useState } from  'react';
+import { SignedIn, SignedOut } from "@clerk/clerk-react"
+import Login from '@components/login';
 import Background from '@assets/osuAstonPlaceApartments.jpeg'
 
 export default function postListing() {
@@ -19,6 +21,8 @@ export default function postListing() {
     const isFormValid = address !== '' && state !== '' && zipCode !== ''  && city !== '' && houseType !== '' && roomsEntered && bathroomsEntered && genderType !== '';
 
     return(
+        <>
+        <SignedIn>
         <div className="bg-gray-200 flex flex-col justify-center items-center w-full h-screen" 
         style={{ 
             backgroundImage: `url(${Background.src})`, 
@@ -223,5 +227,11 @@ export default function postListing() {
 
             </div>
         </div>
+        </SignedIn>
+        
+        <SignedOut>
+            <Login/>
+        </SignedOut>
+        </>
     );
 }
