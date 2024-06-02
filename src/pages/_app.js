53977da2fd0612ec2487eb13d3f1d52c2106153a
Dylan Jian <138734979+dylanjian0@users.noreply.github.com>
2024-06-02 16:30:20 -0400

    Revert "migrate to clerk auth"

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,34 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
 import Layout from '@components/layout';
+import { Auth0Provider } from '@auth0/auth0-react';
 import '../globals.css';  
 
 export default function MyApp({ Component, pageProps }) {
+    const [redirectUri, setRedirectUri] = useState('');
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+        setRedirectUri(window.location.origin);
+        }
+    }, []);
+
+    if (!redirectUri) {
+        return <div>Loading...</div>;
+      }
 
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <Auth0Provider
+        domain = {process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+        clientId = {process.env.NEXT_PUBLIC_AUTH0_CLIENTID}
+        authorizationParams={{
+            redirect_uri: redirectUri,
+        }}
+        cacheLocation="localstorage"
+        useRefreshTokens={true}
+        >
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </Auth0Provider>
     );
 }
