c5a790b99d964a268e4d90aaf79ffce3b3eac6c8
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-06 02:37:38 +0900

    navbar profile styling

--- src/components/login.js
+++ src/components/login.js
@@ -10,7 +10,7 @@ function LoginButton() {
   }
 
   return isAuthenticated ? (
-    <Link href='/profile' className="flex items-center flex-row space-x-2 font-bold transition duration-300 ease-in-out hover:text-white">
+    <Link href='/profile' className="flex items-center flex-row space-x-2 font-bold transition duration-300 ease-in-out text-black bg-white rounded-2xl px-4 py-2">
       <img src={user.picture} alt={user.name} className="w-6 h-6 rounded-full" />
       <p className="font-bold transition duration-300 ease-in-out hover:text-gray-400"> {user.nickname} </p>
     </Link>
