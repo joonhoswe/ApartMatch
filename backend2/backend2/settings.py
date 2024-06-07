50b6afbf6bfc856897d7927006fb0b9607f9725a
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-07 23:21:07 +0900

    updated requirements.txt and secured .env

--- backend2/backend2/settings.py
+++ backend2/backend2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('NEXT_PUBLIC_DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -93,15 +93,15 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'railway',
+        'NAME': os.environ.get('DB_NAME'),
 
-        'USER': 'postgres',
+        'USER': os.environ.get('DB_USER'),
 
-        'PASSWORD': 'aDTBimLNphHQnmusfVZumTsVanhWWJlK',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
 
-        'HOST': 'roundhouse.proxy.rlwy.net',
+        'HOST': os.environ.get('DB_HOST'),
 
-        'PORT': '30616',
+        'PORT': os.environ.get('DB_PORT'),
 
     }
 }
