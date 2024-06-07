726652d2997d9ec6dea8fbc94c5b306d2e1b6f46
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-07 23:14:20 +0900

    migrated to and deployed postgres

--- backend2/backend2/settings.py
+++ backend2/backend2/settings.py
@@ -90,8 +90,19 @@ WSGI_APPLICATION = 'backend2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'railway',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'aDTBimLNphHQnmusfVZumTsVanhWWJlK',
+
+        'HOST': 'roundhouse.proxy.rlwy.net',
+
+        'PORT': '30616',
+
     }
 }
 
