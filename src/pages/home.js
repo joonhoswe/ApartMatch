337732dacd81191fb7af818b8607674e9bc421ad
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-07 22:15:50 +0900

    home page styling improvements esp. for mobile

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,15 +8,15 @@ export default function Home() {
 
   const infoCards = [
     { 
-      heading: '❌ Online housing groups are inefficient', 
+      heading: '❌ Online housing forums? Yuck.', 
       description: 'Say goodbye to sifting through outdated, irrelevant, and scam-filled posts. ApartMatch shows only listings by other students with extra rooms looking to be filled, enabling students to swiftly match into rooms.' 
     },
     { 
-      heading: '✅ See openings for apartments in real time', 
-      description: 'Stay up-to-date with real-time availability. Instantly view and join partially filled apartments, making it easier and faster to save money while finding your ideal living situation. Apartmatch\'s matches automatically close once filled.' 
+      heading: '✅ See openings in real time', 
+      description: 'Stay up-to-date with real-time availability. Instantly view and join partially filled apartments, making it easier and faster to save money while finding your ideal living situation.' 
     },
     { 
-      heading: '🔒 Only students with .edu emails are allowed', 
+      heading: '🔒 Only .edu emails allowed', 
       description: 'Safety and trust are our priorities. That\'s why only users with valid .edu email addresses can join and post, guaranteeing that you\'re matched with verified college students from your own school.' 
     },
   ];
@@ -30,18 +30,21 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center w-full h-[calc(100vh-54px)] text-white font-bold">
       <div 
-        className='h-full w-full flex flex-col space-y-4 justify-between items-center py-12' 
+        className='h-full w-full flex flex-col space-y-12 md:space-y-4 justify-between items-center py-12' 
         style={{ 
           backgroundImage: `url(${Apartment.src})`, 
           backgroundSize: 'cover',
           backgroundPosition: 'center'
         }}
       >
-        <div className='text-md sm:text-3xl md:text-4xl flex flex-col space-y-1 sm:space-y-3 md:space-y-6 items-center'>
-          <h1> 🐌 No more inefficient housing blogs. </h1>
+
+        {/* titles */}
+        <div className='text-md sm:text-3xl md:text-4xl flex flex-col space-y-1 sm:space-y-3 md:space-y-6 text-center items-center'>
+          <h1> 🐌 No more old housing forums. </h1>
           <h1> 🔥 Instantly <span className='text-red-500 underline'> match</span> into apartments. </h1>
         </div>
 
+        {/* school search bar, passes string to Geocoder API for Google Map search */}
         <div className='flex bg-white h-10 md:h-12 w-3/4 md:w-1/2 rounded-lg md:rounded-2xl'>
           <input 
             className={`h-full w-5/6 rounded-l-lg md:rounded-l-2xl px-4 text-black outline-none text-xs sm:text-sm lg:text-base`} 
@@ -63,11 +66,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='bg-gray-200 h-screen w-full flex flex-col md:flex-row space-y-12 md:space-y-0 md:space-x-6 lg:space-x-12 items-center justify-center py-10'>
+      <div className='bg-gray-200 w-full flex flex-col md:flex-row space-y-12 md:space-y-0 md:space-x-6 lg:space-x-12 items-center justify-center px-4 py-10'>
         {infoCards.map((card, index) => (
-          <div key={index} className='bg-white text-black h-64 md:h-80 w-64 md:w-80 rounded-2xl flex flex-col items-center space-y-4 p-4 md:p-6 shadow-2xl'>
+          <div key={index} className='bg-white text-black h-56 md:h-72 w-64 md:w-80 rounded-2xl flex flex-col items-center space-y-4 p-4 md:p-6 shadow-2xl'>
             <h1 className='text-sm md:text-xl lg:text-2xl'> {card.heading} </h1>
-            <p className='text-xs md:text-sm lg:text-base font-normal'> {card.description} </p>
+            <p className='text-sm md:text-sm lg:text-base font-normal'> {card.description} </p>
           </div>
         ))}
       </div>
