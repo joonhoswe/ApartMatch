c9b065e27100b330d6241a782f1669f0b671a5f8
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-07 22:05:04 +0900

    view listings card styling update

--- src/components/viewListings.js
+++ src/components/viewListings.js
@@ -31,26 +31,28 @@ export default function ViewListings(){
         };
         fetchData();
     }, [user]);
+
     return (
         <div className='h-[calc(100vh-54px)] w-full md:w-3/4 md:flex flex-col items-center space-y-6 text-black p-2 border-2 border-gray-500'>
-            {isAuthenticated ? ( 
+            {isAuthenticated ? (
               <></>
-            ) : ( 
+            ) : (
             <div className='w-full h-10 bg-white flex items-center justify-center'>
                 <p className='font-bold text-red-500'> Please <span onClick={loginWithRedirect} className='underline hover:cursor-pointer hover:text-red-400 transition ease-in-out duration-300'>sign in </span> to join a listing. </p>
-            </div>   
+            </div>
             )}
-            <div className='h-full w-full overflow-y-scroll grid grid-cols-1 lg:grid-cols-2 gap-12 p-4 items-center justify-items-center'>
+            <div className='h-full w-full overflow-y-scroll grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-12 p-4 items-center justify-items-center'>
                     {database.map((listing, index) => (
-                        <div key={index} className='flex items-center flex-col h-36 w-48 rounded-2xl shadow-2xl hover:cursor-pointer hover:scale-105 transition ease-in-out duration-300'>
-                            <img src = {placeholder.src} alt = 'placeholder' className='h-16 w-24'/>
-                            <div className='flex flex-col space-y-1 justify-center items-start'>
-                                <div className='flex flex-row space-x-1 items-center justify-center'>
-                                <h1 className='text-sm font-bold'> ${listing.rent}/mo </h1>
-                                <p className='text-xs'> {listing.rooms} bed {listing.bathrooms} bath </p>
+                        <div key={index} className='flex items-center flex-col h-48 w-48 rounded-2xl shadow-2xl hover:cursor-pointer hover:scale-105 transition ease-in-out duration-300'>
+                            <img src = {placeholder.src} alt = 'placeholder' className='h-24 w-full'/>
+                            <div className='flex flex-col space-y-1 justify-start text-start px-4'>
+                                <div className='flex flex-row space-x-1 items-center'>
+                                  <h1 className='text-sm font-bold'> ${listing.rent}/mo </h1>
+                                  <p className='text-xs'> {listing.rooms} bed, {listing.bathrooms} bath </p>
                                 </div>
                                 <p className='text-xs'> {listing.address} </p>
-                                <p className='text-xs'> {listing.city}, {listing.state} {listing.zipCode}</p>
+                                <p className='text-xs'> {listing.city}, {listing.state}, {listing.zipCode}</p>
+                                <button className='text-red-500 text-xs font-bold transition ease-in-out duration-300 hover:text-gray-400'> Join </button>
                             </div>
                         </div>
                     ))}
