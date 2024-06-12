fb3b6a1e7949ef40e65ae72e99c834df9b2a00b9
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-12 23:02:17 +0900

    popup styling fixes

--- src/components/listingPopup.js
+++ src/components/listingPopup.js
@@ -7,15 +7,15 @@ export default function ListingPopup({ listing }) {
 
     return (
         <div className='z-50 w-full bg-white text-black rounded-md flex flex-col items-center p-4'>
-            <img src={placeholder.src} alt='placeholder' className='h-32 w-full mb-2' />
+            <img src={placeholder.src} alt='placeholder' className='h-40 w-full mb-2' />
             <div className='flex flex-col space-y-1 justify-start text-start w-full px-4'>
                 <div className='flex flex-row space-x-1 items-center'>
-                    <h1 className='text-sm font-bold'>${listing.rent}/mo</h1>
-                    <p className='text-xs'>{listing.rooms} bed, {listing.bathrooms} bath</p>
+                    <h1 className='text-lg font-bold'>${listing.rent}/mo</h1>
+                    <p className='text-base'>{listing.rooms} bed, {listing.bathrooms} bath</p>
                 </div>
-                <p className='text-xs font-bold text-green-500'>{listing.rooms - listing.joinedListing.length} / {listing.rooms} Rooms Open</p>
-                <p className='text-xs'>{listing.address}</p>
-                <p className='text-xs'>{listing.city}, {listing.state}, {listing.zipCode}</p>
+                <p className='text-base font-bold text-green-500'>{listing.rooms - listing.joinedListing.length} / {listing.rooms} Rooms Open</p>
+                <p className='text-sm'>{listing.address}</p>
+                <p className='text-sm'>{listing.city}, {listing.state}, {listing.zipCode}</p>
                 {isAuthenticated && listing.joinedListing.includes(user.nickname)
                     ? <p className='text-gray-400 text-xs font-bold text-center'>Already Joined</p>
                     : isAuthenticated && !listing.joinedListing.includes(user.nickname) ? <button onClick={() => handleJoin(listing.id, user.nickname)} className='text-red-500 text-xs font-bold transition ease-in-out duration-300 hover:text-gray-400'>Join</button>
