07c7d7d6c3a9c7673fdd68d2d882d0467efa69a7
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-20 02:48:17 -0400

    added loading spinners

--- src/components/listingPopup.js
+++ src/components/listingPopup.js
@@ -2,28 +2,35 @@ import React, { useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import placeholder from '@assets/placeholder.jpeg';
 import axios from 'axios';
+import { TailSpin } from 'react-loader-spinner';
 
 export default function ListingPopup({ listing, refreshListing }) {
     const { user, isAuthenticated } = useAuth0();
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [confirm, setConfirm] = useState(false);
 
     const handleJoin = async (id, user) => {
+        setLoading(true);
         try {
             await axios.patch('http://localhost:8000/api/join/', { id, user });
             await refreshListing(); // Re-fetch listings after join
         } catch (error) {
             console.error('Error joining listing:', error);
         }
+        setLoading(false);
+        setConfirm(false);
     };
 
     const handleLeave = async (id, user) => {
+        setLoading(true);
         try {
             await axios.patch('http://localhost:8000/api/leave/', { id, user });
             await refreshListing(); // Re-fetch listings after leave
         } catch (error) {
             console.error('Error leaving listing:', error);
         }
+        setLoading(false);
+        setConfirm(false);
     };
 
     return (
@@ -45,31 +52,31 @@ export default function ListingPopup({ listing, refreshListing }) {
             {
                 isAuthenticated && listing.joinedListing.includes(user.nickname) ? 
                 <div className=' h-12 flex flex-col space-y-2 items-center justify-center'>
-                    <p className={`text-xs font-bold text-red-500 ${confirm ? 'block' : 'hidden'}`}> Are you sure you want to leave this listing? </p>
+                    <p className={`text-sm font-bold text-red-500 ${confirm ? 'block' : 'hidden'}`}> Are you sure you want to leave this listing? </p>
                     <div className={`flex flex-row space-x-8 ${confirm ? 'block' : 'hidden'}`}>
-                        <button onClick={() => {handleLeave(listing.id, user.nickname); setConfirm(false)}} className='w-16 h-6 outline-none ring-2 ring-red-500 bg-red-500 text-white hover:bg-white hover:text-red-500 text-sm font-bold transition ease-in-out duration-300 rounded-lg'> 
-                            Confirm 
-                        </button>
-                        <button onClick={() => setConfirm(false)} className='w-16 h-6 outline-none ring-2 ring-gray-500 bg-gray-500 text-white hover:bg-white hover:text-gray-500 text-sm font-bold transition ease-in-out duration-300 rounded-lg'> 
+                        <button onClick={() => setConfirm(false)} className='text-gray-500 text-sm font-bold'> 
                             Cancel 
                         </button>
+                        <button onClick={() => handleLeave(listing.id, user.nickname)} className='flex items-center justify-center w-20 h-6 outline-none ring-2 ring-red-500 bg-red-500 text-white text-sm font-bold hover:scale-105 transition ease-in-out duration-300 rounded-lg'> 
+                            {loading ? <TailSpin color='#ffffff' height={15} width={15} /> : 'Confirm'}
+                        </button>
                     </div>
-                    <button onClick={() => setConfirm(true)} className={`w-16 h-6 outline-none ring-2 ring-red-500 bg-red-500 text-white hover:bg-white hover:text-red-500 text-sm font-bold transition ease-in-out duration-300 rounded-lg ${confirm ? 'hidden' : 'block'}`}> 
+                    <button onClick={() => setConfirm(true)} className={`w-16 h-6 outline-none ring-2 ring-red-500 bg-red-500 text-white text-sm font-bold hover:scale-105 transition ease-in-out duration-300 rounded-lg ${confirm ? 'hidden' : 'block'}`}> 
                         Leave 
                     </button>
                 </div>
                 : isAuthenticated && !listing.joinedListing.includes(user.nickname) ? 
                 <div className=' h-12 flex flex-col space-y-2 items-center justify-center'>
-                    <p className={`text-xs font-bold text-green-500 ${confirm ? 'block' : 'hidden'}`}> Join this listing? </p>
+                    <p className={`text-sm font-bold text-green-500 ${confirm ? 'block' : 'hidden'}`}> Join this listing? </p>
                     <div className={`flex flex-row space-x-8 ${confirm ? 'block' : 'hidden'}`}>
-                        <button onClick={() => {handleJoin(listing.id, user.nickname); setConfirm(false)}} className='w-16 h-6 outline-none ring-2 ring-green-500 bg-green-500 text-white hover:bg-white hover:text-green-500 text-sm font-bold transition ease-in-out duration-300 rounded-lg'> 
-                            Confirm 
-                        </button>
-                        <button onClick={() => setConfirm(false)} className='w-16 h-6 outline-none ring-2 ring-gray-500 bg-gray-500 text-white hover:bg-white hover:text-gray-500 text-sm font-bold transition ease-in-out duration-300 rounded-lg'> 
+                    <button onClick={() => setConfirm(false)} className='text-gray-500 text-sm font-bold'> 
                             Cancel 
                         </button>
+                        <button onClick={() => handleJoin(listing.id, user.nickname)} className='flex items-center justify-center w-20 h-6 outline-none ring-2 ring-green-500 bg-green-500 text-white text-sm font-bold hover:scale-105 transition ease-in-out duration-300 rounded-lg'> 
+                            {loading ? <TailSpin color='#ffffff' height={15} width={15} /> : 'Confirm'}
+                        </button>
                     </div>
-                    <button onClick={() => setConfirm(true)} className={`w-16 h-6 outline-none ring-2 ring-green-500 bg-green-500 text-white hover:bg-white hover:text-green-500 text-sm font-bold transition ease-in-out duration-300 rounded-lg ${confirm ? 'hidden' : 'block'}`}> 
+                    <button onClick={() => setConfirm(true)} className={`w-16 h-6 outline-none ring-2 ring-green-500 bg-green-500 text-white text-sm font-bold hover:scale-105 transition ease-in-out duration-300 rounded-lg ${confirm ? 'hidden' : 'block'}`}> 
                         Join 
                     </button>
                 </div>
