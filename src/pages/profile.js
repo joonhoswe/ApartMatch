03e8537e0c6825ba59b3ef6696264414bf373591
dylanjian0 <dylanjian0@gmail.com>
2024-06-20 16:04:12 -0400

    owner badge issue

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -18,7 +18,7 @@ export default function Profile() {
           const response = await axios.get('http://localhost:8000/api/get');
           setDatabase(response.data);
           if (user) {
-            const userOwnedListings = response.data.filter(listing => listing.owner == user.nickname);
+            const userOwnedListings = response.data.filter(listing => listing.joinedListing.includes(user.nickname));
             setListings(userOwnedListings);
           }
           setLoading(false); // Set loading to false after the data has been fetched
@@ -81,9 +81,11 @@ export default function Profile() {
             <div className='flex gap-4 items-center flex-wrap'>
             {listings.map((listing, index) => (
                 <div key={index} className='relative flex flex-col h-56 w-48 rounded-2xl shadow-2xl hover:cursor-pointer hover:scale-110 transition ease-in-out duration-300'>
-                    <div className='absolute top-2 left-2 bg-yellow-400 text-white font-bold text-xs w-18 h-4 p-1 rounded-md flex items-center justify-center'> 
+                    {listing.owner === user.nickname ? (
+                      <div className='absolute top-2 left-2 bg-yellow-400 text-white font-bold text-xs w-18 h-4 p-1 rounded-md flex items-center justify-center'> 
                       <p className=''>☆OWNER </p>
-                    </div>
+                      </div>
+                    ):<></>}
                     <img src = {placeholder.src} alt = 'placeholder' className='h-24 w-full rounded-t-2xl'/>
                     <div className='flex flex-col space-y-1 justify-start text-start px-4'>
                         <div className='flex flex-row space-x-1 items-center'>
