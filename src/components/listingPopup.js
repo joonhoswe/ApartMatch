d66f4900e086d0d0e2d7b749ba7ed3652959dd8d
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-06-30 01:06:41 -0400

    added visuals for available and taken listings

--- src/components/listingPopup.js
+++ src/components/listingPopup.js
@@ -4,6 +4,8 @@ import placeholder from '@assets/placeholder.jpeg';
 import axios from 'axios';
 import { TailSpin } from 'react-loader-spinner';
 import AWS from 'aws-sdk';
+import taken from '@assets/taken.png';
+import available from '@assets/available.png';
 
 export default function ListingPopup({ allListings, listing, refreshListing, changePopupActive, changeUserListing }) {
     const { user, isAuthenticated } = useAuth0();
@@ -111,11 +113,11 @@ export default function ListingPopup({ allListings, listing, refreshListing, cha
                 x
             </button>
             <div className='h-3/5 w-full mb-2 relative'>
-                <button onClick={handlePrev} className='z-10 absolute top-1/2 left-4 h-9 w-9 md:h-12 md:w-12 rounded-full bg-white text-black flex items-center justify-center text-base md:text-2xl font-bold shadow-2xl hover:bg-red-500 transition ease-in-out duration-300'> 〈  </button>
+                <button onClick={handlePrev} className='z-10 absolute top-1/2 left-4 h-6 w-6 md:h-9 md:w-9 rounded-full bg-white text-black flex items-center justify-center text-base md:text-2xl font-bold shadow-2xl hover:bg-red-500 transition ease-in-out duration-300'> 〈  </button>
                 
                 <img src={!listing.images[activeImage] ? placeholder.src : listing.images[activeImage]} alt='Listing Image' className='h-96 w-160' />
 
-                <button onClick={handleNext} className='z-10 absolute top-1/2 right-4 h-9 w-9 md:h-12 md:w-12 rounded-full bg-white text-black flex items-center justify-center text-base md:text-2xl font-bold shadow-2xl hover:bg-red-500 transition ease-in-out duration-300'> 〉  </button>
+                <button onClick={handleNext} className='z-10 absolute top-1/2 right-4 h-6 w-6 md:h-9 md:w-9 rounded-full bg-white text-black flex items-center justify-center text-base md:text-2xl font-bold shadow-2xl hover:bg-red-500 transition ease-in-out duration-300'> 〉  </button>
             
                 <div className='z-50 absolute bottom-2 left-1/2 flex flex-row space-x-2 items-center justify-center'>
                     { listing.images.map((exp, index) => (
@@ -130,12 +132,32 @@ export default function ListingPopup({ allListings, listing, refreshListing, cha
                         <h1 className='text-lg font-bold'>${listing.rent}/mo</h1>
                         <p className='text-base'>{listing.rooms} bed, {listing.bathrooms} bath</p>
                     </div>
-                    <p className='text-base font-bold text-green-500'> {listing.rooms - listing.joinedListing.length} / {listing.rooms} Rooms Open </p>
+
+                    <div className='flex flex-row space-x-1 items-center'>
+                        {Array.from({ length: listing.rooms - listing.joinedListing.length }).map((_, index) => (
+                            <div key={index} className='flex flex-row space-x-1 items-center'>
+                                <img src={available.src} alt='available' className="w-3 h-6" />
+                            </div>
+                        ))}
+                        {listing.joinedListing.map((user, index) => (
+                            <div key={index} className='flex flex-row space-x-1 items-center'>
+                                <img src={taken.src} alt='taken' className="w-3 h-6" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                
+                <div className='flex justify-between'>
+                    <p className='text-sm items-center'> {listing.address} <br/> {listing.city}, {listing.state}, {listing.zipCode} </p>
+                    <div className='flex flex-col text-right'>
+                        <p className='text-base font-bold text-green-500'> {listing.rooms - listing.joinedListing.length} / {listing.rooms} Rooms Open </p>
+                        <p className={`${listing.gender === 'males' ? 'text-blue-500' : listing.gender === 'females' ? 'text-pink-500' : 'text-gray-500'} font-bold text-base`}> {listing.gender} </p>
+                    </div>
+                </div>
+                <div className='flex items-center justify-between'>
+                    <p className='text-sm'> Unit #: {listing.homeType === 'apartment' ? listing.unit: 'N/A' } </p>
                 </div>
 
-                
-                <p className='text-sm'>{listing.address} <br/> {listing.city}, {listing.state}, {listing.zipCode}</p>
-                <p className='text-sm'> Unit #: {listing.homeType === 'apartment' ? listing.unit: 'N/A' } </p>
             </div>
 
             {   
