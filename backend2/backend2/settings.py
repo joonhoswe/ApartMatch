d25ccd2547ddf32ac124d2f7165b6fb08d7e4fa4
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-07-02 15:43:00 -0400

    deploying backend

--- backend2/backend2/settings.py
+++ backend2/backend2/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'app',
     'corsheaders',
     'rest_framework',
+    'whitenoise.runserver_nostatic',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -55,6 +56,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -142,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
