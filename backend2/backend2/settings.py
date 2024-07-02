f03fd24d25daff44a03968125ee5a5e4e098b05a
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-07-02 16:48:12 -0400

    settings.py updates

--- backend2/backend2/settings.py
+++ backend2/backend2/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOST')]
 
 
 # Application definition
