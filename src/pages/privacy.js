51aace43d3ffd419250e26d92f19d6a8a08f5db8
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-07-05 21:00:55 -0400

    added terms and privacy

---
+++ src/pages/privacy.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+export default function Privacy() {
+
+    return (
+        <div className="w-full h-screen overflow-auto bg-white flex flex-col items-start justify-start p-4 py-8 md:px-8 md:py-16 text-black">
+
+            <h1 className="text-2xl md:text-3xl font-bold text-black mb-2"> Privacy Policy </h1>
+            <p className='text-sm md:text-base'> 
+            At ApartMatch, we are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website and use our services. Please read this Privacy Policy carefully. If you do not agree with the terms of this Privacy Policy, you are prohibited from using or accessing this site.
+            </p>
+
+            <h1 className="text-2xl md:text-3xl font-bold text-black mb-2 mt-4"> Information We Collect </h1>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-2"> Personal Information </h2>
+            <p className='text-sm md:text-base'> 
+            When you register on ApartMatch and use our services, we collect personal information that you voluntarily provide to us. This includes: <br/>
+            <span className='font-bold text-sm md:text-base'> Username: </span>Collected and securely stored through Auth0 during account creation. <br/>
+            <span className='font-bold text-sm md:text-base'> Email: </span>Collected and securely stored through Auth0 during account creation.  <br/>
+            <span className='font-bold text-sm md:text-base'> Password: </span>Collected and securely stored through Auth0 during account creation. 
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Listing Information </h2>
+            <p className='text-sm md:text-base'> 
+            When you post a home listing on ApartMatch, we collect the following information: <br/>
+            <span className='font-bold text-sm md:text-base'> Address: </span>The street address of the property. <br/>
+            <span className='font-bold text-sm md:text-base'> Listing Details: </span>Information about the property, such as the number of rooms, rental price, owner's gender preferences, and other relevant details.  <br/>
+            <span className='font-bold text-sm md:text-base'> Images: </span>Photos of the property.
+            </p>
+
+            <h1 className="text-2xl md:text-3xl font-bold text-black mb-2 mt-4"> How We Use Your Information </h1>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-2"> Personal Information </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> To Create and Manage Your Account: </span>We use your username, email, and password to create and manage your account through Auth0. <br/>
+            <span className='font-bold text-sm md:text-base'> To Connect You With Your Future Roommates: </span>We use your email address to connect you with other users in the same listing for communication and coordination. <br/>
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Listing Information </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> To Display Listings: </span>We use the address, listing details, and images you provide to display your listing on ApartMatch. <br/>
+            <span className='font-bold text-sm md:text-base'> To Facilitate Matching: </span>We use the information in your listing to help match you with potential roommates or tenants.
+            </p>
+
+            <h1 className="text-2xl md:text-3xl font-bold text-black mb-2 mt-4"> Sharing Your Information </h1>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-2"> Personal Information </h2>
+            <p className='text-sm md:text-base'> 
+            We do not sell, trade, or otherwise transfer your personal information to outside parties, except as described in this Privacy Policy or with your consent. We may share your personal information with: <br/>
+            <span className='font-bold text-sm md:text-base'> Service Providers: </span>Third-party service providers who assist us in operating our website and conducting our business, such as Auth0 for authentication services.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Listing Information </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> Public Listings: </span>The address, details, and images of your listings are publicly displayed on ApartMatch to facilitate the matching process. <br/>
+            <span className='font-bold text-sm md:text-base'> Potential Matches: </span>Your email address may be shared with users who are interested in joining your listing to facilitate communication.
+            </p>
+
+            <h1 className="text-2xl md:text-3xl font-bold text-black mb-2 mt-4"> Cookies and Tracking Technologies </h1>
+            <p className='text-sm md:text-base'> 
+            We may use cookies and similar tracking technologies to track activity on our site and hold certain information. Cookies are files with a small amount of data which may include an anonymous unique identifier. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our site.
+            </p>
+
+            <h1 className="text-2xl md:text-3xl font-bold text-black mb-2 mt-4"> Data Retention </h1>
+            <p className='text-sm md:text-base'> 
+            We will retain your personal information for as long as your account is active or as needed to provide you services, comply with our legal obligations, resolve disputes, and enforce our agreements. Listing information will be retained as long as the listing is active on ApartMatch.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Changes to Privacy Policy </h2>
+            <p className='text-sm md:text-base'> 
+            We reserve the right to modify our Privacy Policy at any time. Any changes will be effective immediately upon posting on the site. Your continued use of ApartMatch after the posting of revised Privacy Policy constitutes your acceptance of the changes.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Contact Us </h2>
+            <p className='text-sm md:text-base'> 
+            If you have any questions or concerns about our Privacy Policy, please contact us at: apartmatchco@gmail.com <br/>
+            By using ApartMatch, you acknowledge that you have read, understood, and agree to be bound by these Terms and our Privacy Policy.
+            </p>
+
+            <p className="text-sm md:text-base text-gray-400 mt-4"> Last updated: July 05 2024 </p>
+        </div>
+    );        
+}
---
+++ src/pages/terms.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+export default function Terms() {
+
+    return (
+        <div className="w-full h-screen overflow-auto bg-white flex flex-col items-start justify-start p-4 py-8 md:px-8 md:py-16 text-black">
+
+            <h1 className="text-2xl md:text-3xl font-bold text-black mb-2"> Terms of Service </h1>
+            <p className='text-sm md:text-base'> 
+            Welcome to ApartMatch! By accessing or using our website, apartmatchco.com, you agree to comply with and be bound by the following terms and conditions ("Terms"). Please read these Terms carefully before using our services. If you do not agree with any part of these Terms, you are prohibited from using or accessing this site.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Acceptance of Terms </h2>
+            <p className='text-sm md:text-base'> 
+            By creating an account, posting a listing, or joining an existing listing on ApartMatch, you agree to abide by these Terms and our Privacy Policy. These Terms apply to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/or contributors of content.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Eligibility </h2>
+            <p className='text-sm md:text-base'> 
+            To use ApartMatch, you must be at least 18 years old and possess the legal authority to enter into binding contracts. By using our services, you represent and warrant that you meet these requirements.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Account Creation </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> Registration: </span>To access certain features of ApartMatch, you must create an account. During registration, you agree to provide accurate, current, and complete information about yourself. <br/>
+            <span className='font-bold text-sm md:text-base'> Account Security: </span>You are responsible for maintaining the confidentiality of your account information, including your password.  <br/>
+            <span className='font-bold text-sm md:text-base'> Account Termination: </span>We reserve the right to suspend or terminate your account at any time, for any reason, including but not limited to violations of these Terms. 
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Posting Listings </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> Eligibility: </span>To join an existing listing, you must have a valid .edu email address to verify your student status. <br/>
+            <span className='font-bold text-sm md:text-base'> Compliance: </span>All listings must comply with applicable laws and regulations. You agree not to post any illegal, misleading, or fraudulent listings. <br/>
+            <span className='font-bold text-sm md:text-base'> Responsibility: </span>You are solely responsible for the content of your listings. ApartMatch is not responsible for any inaccuracies or misrepresentations in listings.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Joining Listings </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> Listing Requirements: </span>When posting a home listing, you must provide accurate and complete information about the property, including but not limited to the address, number of rooms, and rental price. If any of the listing details change, it is your responsibility to inform currently joined members(if applicable), delete the current listing, and post the updated listing. <br/>
+            <span className='font-bold text-sm md:text-base'> Communication: </span>By joining a listing, you agree to share your email address with other users in the same listing for the purpose of communication and coordination. <br/>
+            <span className='font-bold text-sm md:text-base'> Matching Process: </span>ApartMatch facilitates the matching process but does not guarantee that you will find a suitable match. Users are responsible for conducting their own due diligence before entering into any rental agreements. <br/>
+            <span className='font-bold text-sm md:text-base'> Post-Matching Process: </span>After matching into a listing, it is completely the user's responsibility to coordinate with the listing owner and other members to finalize the rental agreement. ApartMatch is not responsible for any disputes or issues that arise during this process.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> User Conduct </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> Prohibited Activities: </span>You agree not to use ApartMatch for any unlawful purpose or to engage in any activity that disrupts or interferes with the functioning of the site. <br/>
+            <span className='font-bold text-sm md:text-base'> Respectful Interaction: </span>You agree to interact respectfully with other users and not to engage in harassment, abuse, or discrimination.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Disclaimers </h2>
+            <p className='text-sm md:text-base'> 
+            <span className='font-bold text-sm md:text-base'> No Warranty: </span>ApartMatch is provided on an "as is" and "as available" basis. We make no warranties, express or implied, regarding the availability, accuracy, or reliability of the site or its content. <br/>
+            <span className='font-bold text-sm md:text-base'> Limitation of Liability: </span>To the fullest extent permitted by law, ApartMatch shall not be liable for any direct, indirect, incidental, consequential, or punitive damages arising out of or related to your use of the site.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Indemnification </h2>
+            <p className='text-sm md:text-base'> 
+            You agree to indemnify, defend, and hold harmless ApartMatch and its affiliates, officers, directors, employees, and agents from and against any claims, liabilities, damages, losses, and expenses, including reasonable attorneys' fees, arising out of or in any way connected with your use of the site, your violation of these Terms, or your violation of any rights of another.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Changes to Terms </h2>
+            <p className='text-sm md:text-base'> 
+            We reserve the right to modify these Terms at any time. Any changes will be effective immediately upon posting on the site. Your continued use of ApartMatch after the posting of revised Terms constitutes your acceptance of the changes.
+            </p>
+
+            <h2 className="text-xl md:text-2xl font-bold text-black mb-2 mt-4"> Contact Us </h2>
+            <p className='text-sm md:text-base'> 
+            If you have any questions or concerns about these Terms, please contact us at: apartmatchco@gmail.com <br/>
+            By using ApartMatch, you acknowledge that you have read, understood, and agree to be bound by these Terms and our Privacy Policy.
+            </p>
+
+            <p className="text-sm md:text-base text-gray-400 mt-4"> Last updated: July 05 2024 </p>
+        </div>
+    );        
+}
