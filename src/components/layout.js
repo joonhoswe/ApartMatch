49c2299c41622f253d0f07b354b262d384775ec6
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-07-05 21:17:38 -0400

    added favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@ export default function Layout({ children }) {
      <Head>
         <title> ApartMatch </title>
         <meta name="description" content="A platform dedicated to connecting college students with active off-campus housing listings where roommates from the same college are searching for housemates. Easily find and join partially filled apartments to save money and avoid the hassle of outdated Facebook threads." />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <Navbar />
       <main>{children}</main>
